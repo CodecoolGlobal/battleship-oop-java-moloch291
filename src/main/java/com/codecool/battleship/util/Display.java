0b300b8e20660b0f524353fe88c725e69079ce1a
dorogimate <dorogimate6@gmail.com>
2021-08-09 16:37:28 +0200

    add all input methods and display class methods (menu, coordinate asking, name asking)

--- src/main/java/com/codecool/battleship/util/Display.java
+++ src/main/java/com/codecool/battleship/util/Display.java
@@ -1,8 +1,26 @@
 package com.codecool.battleship.util;
 
+import com.codecool.battleship.ships.ShipType;
+
 public class Display {
     public void printMenu() {
-        System.out.println("");
+        System.out.println(
+                "Choose one of the menu points: \n" +
+                           "1: Play game \n" +
+                           "2: Quit \n");
+    }
+
+    public void askForCoordinates() {
+        System.out.println(
+                "Enter a coordinate: \n");
+    }
+
+    public void askForCoordinates(ShipType type) {
+        String shipTypeName = type.getName();
+        String shipLength = type.getSize();
+        System.out.println(
+                "You are placing a" + shipTypeName + "which is" + shipLength + "long. Enter the starting coordinate:" +
+                        "of your ship: \n");
     }
 
     public void printBoard() {
@@ -18,6 +36,6 @@ public class Display {
     }
 
     public void askForName() {
-        System.out.println("");
+        System.out.println("Enter a name: \n");
     }
 }
