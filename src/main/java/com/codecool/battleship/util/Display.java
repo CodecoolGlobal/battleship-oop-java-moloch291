c860441ebf959ee2e4ef454dd446e9fdc33a8f01
dorogimate <dorogimate6@gmail.com>
2021-08-09 17:43:03 +0200

    add printBoard and createTableHeader methods

--- src/main/java/com/codecool/battleship/util/Display.java
+++ src/main/java/com/codecool/battleship/util/Display.java
@@ -1,5 +1,6 @@
 package com.codecool.battleship.util;
 
+import com.codecool.battleship.board.Board;
 import com.codecool.battleship.ships.ShipType;
 
 public class Display {
@@ -23,8 +24,37 @@ public class Display {
                         "of your ship: \n");
     }
 
-    public void printBoard() {
-        System.out.println("");
+    public void printBoard(Board board) {
+        StringBuilder output = new StringBuilder();
+        String tableHeader = createTableHeader(board.getSize());
+        for (int row = 0; row < board.getSize(); row++) {
+            output.append(row + 1);
+            if (row + 1 < 10) output.append(" ");
+            for (int column = 0; column < board.getSize(); column++) {
+                if (board[row][column].getSquareStatus.equals(EMPTY)) output.append("|_").append("__");
+                else {
+                    if (board[row][column].getSquareStatus.equals(SHIP))) {
+                        output.append("|_").append("#_");
+                    } else if (board[row][column].getSquareStatus.equals(HIT))){
+                        output.append("|_").append("H_");
+                    } else if (board[row][column].getSquareStatus.equals(MISS))) {
+                        output.append("|_").append("M_");
+                    }
+                }
+            }
+            output.append("|\n");
+        }
+        System.out.println(tableHeader);
+        System.out.println(output);
+    }
+
+    private String createTableHeader(int boardSize) {
+        StringBuilder tableHeader = new StringBuilder();
+        tableHeader.append("Player ").append(activePlayer).append("'s turn!\n").append("    ");
+        for (int index = 0; index < boardSize; index++) {
+            tableHeader.append(alphabetString.charAt(index)).append("   ");
+        }
+        return tableHeader.toString();
     }
 
     public void printGameplay() {
